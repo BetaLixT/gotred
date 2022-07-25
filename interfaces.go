8678e2446a03d85d82a41a887154a7cb992cb502
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-07-25 18:43:28 +0400

    implemented hooks

---
+++ interfaces.go
@@ -0,0 +1,20 @@
+package gotred
+
+import (
+	"context"
+	"time"
+)
+
+type ITracer interface {
+	TraceDependency(
+		ctx context.Context,
+		spanId string,
+		dependencyType string,
+		serviceName string,
+		commandName string,
+		success bool,
+		startTimestamp time.Time,
+		eventTimestamp time.Time,
+		fields map[string]string,
+	)
+}
--- traceHook.go
+++ traceHook.go
@@ -1,7 +1,102 @@
 package gotred
 
-type TraceHook struct {
+import (
+	"context"
+	"fmt"
+	"time"
 
+	"github.com/go-redis/redis/v8"
+)
+
+const (
+	START_TIME_KEY = "gtrd:start"
+)
+
+type TraceHook struct {
+	tracer ITracer
+	serviceName string
 }
 
+var _ redis.Hook = (*TraceHook)(nil)
+
+func (hook *TraceHook) BeforeProcess(
+	ctx context.Context,
+	cmd redis.Cmder,
+) (context.Context, error) {
+	return context.WithValue(ctx, START_TIME_KEY, time.Now()), nil
+}
+
+func (hook *TraceHook) AfterProcess(
+	ctx context.Context,
+	cmd redis.Cmder,
+) error {
+	end := time.Now()
+	start := end
+	raw := ctx.Value(START_TIME_KEY)
+	if raw != nil {
+		if cstd, ok := raw.(time.Time); ok {
+			start = cstd
+		}
+	}
+
+	fields := map[string]string{}
+	err := cmd.Err()	
+	if err != nil {
+		fields["error"] = err.Error()
+	}
+	hook.tracer.TraceDependency(
+		ctx,
+		"",
+		"Redis",
+		hook.serviceName,
+		cmd.String(),
+		err == nil,
+		start,
+		end,
+		fields,
+	)
+	return nil
+}
+
+func (hook *TraceHook) BeforeProcessPipeline(
+	ctx context.Context,
+	cmds []redis.Cmder,
+) (context.Context, error) {
+	return context.WithValue(ctx, START_TIME_KEY, time.Now()), nil
+}
+func (hook *TraceHook) AfterProcessPipeline(
+	ctx context.Context,
+	cmds []redis.Cmder,
+) error {
+	end := time.Now()
+	start := end
+	raw := ctx.Value(START_TIME_KEY)
+	if raw != nil {
+		if cstd, ok := raw.(time.Time); ok {
+			start = cstd
+		}
+	}
+	fields := map[string]string{}
+	cmdstr := ""
+	for idx, cmd := range cmds {
+		err := cmd.Err()
+		if err != nil {
+			fields[fmt.Sprintf("error:%d", idx)] = err.Error()
+			
+		}
+		cmdstr = cmdstr + cmd.String() + ";"
+	}
+	hook.tracer.TraceDependency(
+		ctx,
+		"",
+		"Redis",
+		hook.serviceName,
+		cmdstr,
+		len(fields) == 0,
+		start,
+		end,
+		fields,
+	)
+	return nil
+}
 
